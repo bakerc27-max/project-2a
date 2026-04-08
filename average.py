b2e87ad5f9ccbf587e5f5ac8dfe5c08fe489098c
bakerc27-max <bakerc27@oregonstate.edu>
2026-04-08 14:58:10 -0400

    Add project files

---
+++ average.py
@@ -0,0 +1,10 @@
+print("Please enter five numbers.")
+
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+
+print("The average of those five numbers is:")
+print(((num_1 + num_2 + num_3 + num_4 + num_5) / 5))
---
+++ temp_convert.py
@@ -0,0 +1,6 @@
+print("Please enter a Celsius temperature.")
+
+celsius_temp = float(input())
+
+print("The equivalent Fahrenheit temperature is:")
+print(((celsius_temp * 1.8) + 32))
